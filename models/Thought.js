d389cba89e626e28783b27da6919ab586d89e9d6
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-05-25 16:24:28 -0400

    added Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -2,6 +2,36 @@ const { Schema, model } = require("mongoose");
 const reactionSchema = require("./Reaction");
 const dateFormat = require("../utils/dateFormat");
 
-const thoughtSchema = new Schema();
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => new Date(timestamp).toLocaleString()
+        },
+        username: {
+            type: String,
+            required: trusted
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: { getters: true },
+        toObject: { getters: true }
+    }
+);
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+// create the Thought Model
+const thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
